87f9dd324554700bfc9c481d5f4484cd57e344c1
rwdevops999 <rwdevops999@gmail.com>
2024-11-12 13:07:37 +0100

    added
    environment variables to application.properties

--- src/main/java/com/tutopedia/backend/StartupInit.java
+++ src/main/java/com/tutopedia/backend/StartupInit.java
@@ -3,6 +3,7 @@ package com.tutopedia.backend;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.tutopedia.backend.services.oci.OsService;
@@ -12,9 +13,26 @@ public class StartupInit {
 	@Autowired
 	OsService osService;
 	
+	@Value("${spring.datasource.url}")
+	private String db_datasource;
+
+	@Value("${spring.datasource.username}")
+	private String db_user;
+
+	@Value("${spring.datasource.password}")
+	private String db_password;
+	
+	private void showDBConnection() {
+		System.out.println("===== DB =====");
+		System.out.println("DATASOURCE: " + db_datasource);
+		System.out.println("USER      : " + db_user);
+		System.out.println("PASSWORD  : " + db_password);
+	}
+	
 	@PostConstruct
 	public void init() {
 		try {
+			showDBConnection();
 			osService.initialize();
 		} catch (Exception e) {
 			System.out.println("INIT EXCEPTION: " + e.getMessage());
