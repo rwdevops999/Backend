3489587c274c9270cfe8d52e3a9fe7d72bceeb5b
rwdevops999 <rwdevops999@gmail.com>
2024-11-17 09:04:37 +0100

    Update TutorialController.java
    added API method to unpublish

--- src/main/java/com/tutopedia/backend/controllers/TutorialController.java
+++ src/main/java/com/tutopedia/backend/controllers/TutorialController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -171,6 +172,16 @@ public class TutorialController {
 		log("publishAllTutorials");
 		
 		publishService.publishAllFiles();
+	}
+
+	@PutMapping("/unpublish")
+	@ResponseStatus(HttpStatus.OK)
+	public void unpublishTutorials(@RequestBody List<Tutorial> tutorials ) {
+		log("unpublishTutorials");
+		
+		tutorials.stream().forEach((tutorial) -> System.out.println("unpublish" + tutorial.getFilename()));
+		
+//		publishService.publishAllFiles();
 	}
 
 	@PutMapping("/publish/{id}")
