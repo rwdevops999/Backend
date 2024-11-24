a632e735e12419130b81e26516fac98e02c4ee4f
rwdevops999 <rwdevops999@gmail.com>
2024-11-24 12:31:39 +0100

    Update SettingRepository.java
    adjust query to named query

--- src/main/java/com/tutopedia/backend/persistence/repository/SettingRepository.java
+++ src/main/java/com/tutopedia/backend/persistence/repository/SettingRepository.java
@@ -2,13 +2,20 @@ package com.tutopedia.backend.persistence.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tutopedia.backend.persistence.model.Setting;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface SettingRepository extends CrudRepository<Setting, Long> {
-	Setting findByKeyAndType(String key, String Type);
+    @Modifying
+	@Transactional
+	@Query("select * from Settings s where s.key = :key and s.type = :type")
+	Setting findByKeyAndType(String key, String type);
 	List<Setting> findByType(String type);
 }
